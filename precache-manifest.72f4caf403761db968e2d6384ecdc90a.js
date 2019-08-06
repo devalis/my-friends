e78056235fe6c976f7975c7ffe57f432752d0820
Sofia Alysandratou <sofia.alys@hitsa.local>
2019-08-06 21:35:26 +0300

    Updates

---
+++ precache-manifest.72f4caf403761db968e2d6384ecdc90a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308fdf0a7f156e78c5ac69957fe84910",
+    "url": "/my-friends/index.html"
+  },
+  {
+    "revision": "ad526444b94c11e83bcd",
+    "url": "/my-friends/static/css/main.49ab48c6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/my-friends/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ad526444b94c11e83bcd",
+    "url": "/my-friends/static/js/main.8139343c.chunk.js"
+  },
+  {
+    "revision": "a8de3abf44860b9658e8",
+    "url": "/my-friends/static/js/runtime~main.e82ab982.js"
+  }
+]);
